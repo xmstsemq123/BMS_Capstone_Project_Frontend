36577aa3f7b13200b086bb750b127eb06ac0d46e
xmstsemq123 <xmstsemq123@gmail.com>
2025-11-26 12:47:05 +0800

    a

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -11,7 +11,7 @@ import RideStatus from './RideStatus/RideStatus';
 
 const Block = ({ title, children, className }) => {
   return (
-    <div className={`bg-[#7a7a7a] rounded-xl shadow-md p-6 text-black ${className}`}>
+    <div className={`bg-[#d6d6d6] rounded-xl shadow-md p-6 text-black ${className}`}>
       <h2 className="text-lg font-semibold mb-2">{title}</h2>
       <div>{children}</div>
     </div>
@@ -25,7 +25,7 @@ export default function Home() {
   const batteryTemperature = useSelector((state) => state.homeData.temperature)
   const batteryVoltage = useSelector((state) => state.homeData.voltage)
   return (<>
-    <div className="p-6 bg-[#7a7a7a] select-none">
+    <div className="p-6 bg-[#d6d6d6] select-none">
       <h1 className="text-2xl font-bold text-black mb-6">電池模組狀態總覽</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         <Block title="電車狀態">
@@ -48,8 +48,8 @@ export default function Home() {
         </Block>
       </div>
     </div>
-    <div className="p-6 bg-[#1f1b2e] mt-6 select-none">
-      <h1 className="text-2xl font-bold text-white mb-6">單一元件狀態總覽</h1>
+    <div className="p-6 bg-[#d6d6d6] mt-6 select-none">
+      <h1 className="text-2xl font-bold text-black mb-6">單一元件狀態總覽</h1>
       <CellActivityBlock />
     </div>
   </>);
